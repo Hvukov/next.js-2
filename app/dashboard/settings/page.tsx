306a271b75abd59ef4d0eb020a5122167f1cce45
Hrvoje Vukov <hrvoje.vukov@dss.corp>
2024-07-06 16:42:13 +0200

    finish chapters up to database

---
+++ app/dashboard/settings/page.tsx
@@ -0,0 +1,8 @@
+export default function Page() {
+    return (
+        <section>
+            <h1>Settings</h1>
+            <p>Settings Page</p>
+        </section>
+    )
+}
---
+++ app/ui/fonts.ts
@@ -0,0 +1,7 @@
+import { Inter } from 'next/font/google';
+import { Acme } from 'next/font/google';
+import { Lusitana } from 'next/font/google';
+ 
+export const inter = Inter({ subsets: ['latin'] });
+export const acme = Acme({ subsets: ['latin'], weight: "400"});
+export const lusitana = Lusitana({ subsets: ['latin'], weight: "400"});
